1c586d8c41f0ed0922623610ad9863c8836b99e7
AlineSotte <alinedpaso@gmail.com>
2019-07-06 22:15:22 -0300

    Refatoração de import

--- src/main/java/br/com/dcc193/trabalhoop/Repositorio/EventoRepositorio.java
+++ src/main/java/br/com/dcc193/trabalhoop/Repositorio/EventoRepositorio.java
@@ -3,6 +3,8 @@ package br.com.dcc193.trabalhoop.Repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.dcc193.trabalhoop.Modelo.Evento;
+
 
 
 /**
