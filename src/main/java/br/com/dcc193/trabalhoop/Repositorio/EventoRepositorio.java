619165551e86d76f9f2eaea1bf506de8cc500168
YanPaiva <gckael@gmail.com>
2019-07-08 21:32:24 -0300

    Criando query de buscar eventos by atendimento

--- src/main/java/br/com/dcc193/trabalhoop/Repositorio/EventoRepositorio.java
+++ src/main/java/br/com/dcc193/trabalhoop/Repositorio/EventoRepositorio.java
@@ -1,8 +1,13 @@
 package br.com.dcc193.trabalhoop.Repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import br.com.dcc193.trabalhoop.Modelo.Atendimento;
 import br.com.dcc193.trabalhoop.Modelo.Evento;
 
 
@@ -13,5 +18,7 @@ import br.com.dcc193.trabalhoop.Modelo.Evento;
 @Repository
 public interface EventoRepositorio extends JpaRepository<Evento,Long> {
 
-    
+    @Query("SELECT e FROM Evento e WHERE e.ATENDIMENTO_ID =:atendimento")
+    List<Evento> getEventosByAtendimento(@Param("atendimento") Atendimento atendimento);
+   
 }
